f704cb69c75d8ea219fef99dd91b52c835e9a0e1
Wail EL ABOUDI <121772821+stoplayer@users.noreply.github.com>
2024-04-19 00:01:26 +0100

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/dto/ReqRes.java
@@ -0,0 +1,34 @@
+package com.example.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.example.backend.entity.OurUsers;
+import com.example.backend.entity.Product;
+import com.example.backend.entity.Pays;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ReqRes {
+
+    private int statusCode;
+    private String error;
+    private String message;
+    private String token;
+    private String refreshToken;
+    private String expirationTime;
+    private String name;
+    private String email;
+    private String role;
+    private String password;
+    private List<Product> products;
+    private OurUsers ourUsers;
+    private Pays pays;
+
+
+}
---
+++ backend/src/main/java/com/example/backend/repository/PaysRepo.java
@@ -0,0 +1,11 @@
+package com.example.backend.repository;
+
+import com.example.backend.entity.Pays;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaysRepo extends JpaRepository<Pays, String> {
+    Optional<Pays> findByNom(String name);
+
+}
